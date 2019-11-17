87f8e7fdf5651b9c8dcd6cf5b73672560d245cf5
Anthony Mai <anthony.mai530@gmail.com>
2019-11-17 01:16:04 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,21 @@
 def first_challenge
-   epic_tragedy = {}
+  epic_tragedy = {
+   :montague => {
+   }, 
+   :capulet => {
+  }
+  }
+end
+
+def second_challenge 
+  epic_tragedy = {
+   :montague => { 
+     :patriarch => {},
+     :matriarch => {},
+     :hero => {}, 
+     :hero_friends => []
+   }, 
+   :capulet => {
+  }
+  }
 end
